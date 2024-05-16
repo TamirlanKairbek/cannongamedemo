7983e7a894b5fef5ca0fb0edfe0bd3c62c6bc156
TamirlanKairbek <kairbektamirlan@gmail.com>
2024-05-16 22:42:45 +0200

    physics&restartadded

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from kivy.uix.widget import Widget
 from kivy.vector import Vector
 from kivy.clock import Clock
 from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.config import Config
+
+Config.set('graphics', 'width', '1000')
+Config.set('graphics', 'height', '700')
 
 
 class PongBall(Widget):
@@ -13,15 +17,22 @@ class PongBall(Widget):
     end_pos = ()  # Конечная позиция натягивания мяча
     is_dragging = False  # Флаг для отслеживания натягивания мяча
     is_launched = False  # Флаг для отслеживания запуска мяча
-
+    acceleration = 0
 
     def __init__(self, **kwargs):
         super(PongBall, self).__init__(**kwargs)
         self.size_hint = (None, None)  # Отключаем автоматическое изменение размера
-        self.size = (50, 50)  # Устанавливаем размер мяча (например, 50x50)
+        self.size = (20, 20)  # Устанавливаем размер мяча (например, 50x50)
 
     def move(self, dt):
-        self.pos = Vector(*self.velocity) + self.pos
+        self.velocity_y -= self.acceleration  # Добавляем гравитацию
+        self.pos = Vector(*self.velocity) + self.pos  # Двигаем мяч
+
+        if self.y > 700 or self.y < 0 or self.x > 1000 or self.x < 0:
+            self.pos = (100, 100)
+            self.velocity = Vector(0, 0)
+            self.acceleration = 0   
+            self.is_launched = False   
 
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos) and not self.is_launched:  # Проверяем, касается ли касание мяча и мяч не был запущен
@@ -39,8 +50,10 @@ class PongBall(Widget):
 
     def move_ball(self):
         direction = Vector(*self.end_pos) - Vector(*self.start_pos)  # Вычисляем направление движения
-        self.velocity = direction * -0.05  # Задаем скорость мяча на основе направления натягивания
+        self.velocity = direction * -0.1  # Задаем скорость мяча на основе направления натягивания
         self.is_launched = True  # Устанавливаем флаг, что мяч был запущен
+        self.acceleration = 0.2
+
 
 class PongApp(App):
     def build(self):
@@ -48,6 +61,7 @@ class PongApp(App):
         ball.pos = (100, 100)  # Устанавливаем начальное положение мяча
         Clock.schedule_interval(ball.move, 1.0 / 60.0)  # Запускаем движение мяча
         return ball
+
 
 if __name__ == '__main__':
     PongApp().run()
