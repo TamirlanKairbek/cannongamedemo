037f24cfcea5a25f10d9ea8e2323754f816d7f23
TamirlanKairbek <kairbektamirlan@gmail.com>
2024-05-16 21:48:41 +0200

    constant moving

--- main.py
+++ main.py
@@ -9,20 +9,24 @@ class PongBall(Widget):
     velocity_x = NumericProperty(0)  # Горизонтальная скорость мяча
     velocity_y = NumericProperty(0)  # Вертикальная скорость мяча
     velocity = ReferenceListProperty(velocity_x, velocity_y)
+    start_pos = ()  # Начальная позиция натягивания мяча
+    end_pos = ()  # Конечная позиция натягивания мяча
+    is_dragging = False  # Флаг для отслеживания натягивания мяча
+    is_launched = False  # Флаг для отслеживания запуска мяча
+
 
     def __init__(self, **kwargs):
         super(PongBall, self).__init__(**kwargs)
         self.size_hint = (None, None)  # Отключаем автоматическое изменение размера
         self.size = (50, 50)  # Устанавливаем размер мяча (например, 50x50)
 
-    def move(self, dt):  # Добавляем аргумент dt (время прошедшее с предыдущего обновления)
+    def move(self, dt):
         self.pos = Vector(*self.velocity) + self.pos
 
     def on_touch_down(self, touch):
-        if self.collide_point(*touch.pos):  # Проверяем, касается ли касание мяча
+        if self.collide_point(*touch.pos) and not self.is_launched:  # Проверяем, касается ли касание мяча и мяч не был запущен
             self.start_pos = touch.pos  # Устанавливаем начальную позицию натягивания мяча
             self.is_dragging = True  # Устанавливаем флаг, что мяч натягивается
-            self.velocity = (0, 0)  # Обнуляем скорость мяча
 
     def on_touch_move(self, touch):
         if self.is_dragging:  # Если мяч натягивается
@@ -36,6 +40,7 @@ class PongBall(Widget):
     def move_ball(self):
         direction = Vector(*self.end_pos) - Vector(*self.start_pos)  # Вычисляем направление движения
         self.velocity = direction * -0.05  # Задаем скорость мяча на основе направления натягивания
+        self.is_launched = True  # Устанавливаем флаг, что мяч был запущен
 
 class PongApp(App):
     def build(self):
